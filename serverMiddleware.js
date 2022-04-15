b774a7fb1858970c7a17582b42dcf98d713af2ab
thuan-nv035 <54436704+thuan-nv035@users.noreply.github.com>
2022-04-15 09:26:09 +0700

    middle ware

---
+++ serverMiddleware.js
@@ -0,0 +1,18 @@
+export default (req, res, next) => {
+  if (req.url.includes('/about')) {
+    //res.status(301).redirect('/');
+    res.writeHead(301,{'Location':'/'}); //redirect về trang chủ
+    res.end(); //kết thúc request
+  }else if(req.url.includes('/test')) {
+    // tra ve json
+    res.writeHead(200, {'Content-Type': 'application/json'})
+   const json  = JSON.stringify({
+      name: 'Nguyen Van A',
+      age:20
+    })
+    res.end(json)
+  }
+  else {
+    next();//điều hướng về trang ban đầu của request
+  }
+}
